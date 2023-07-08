b34ec35f855b7658e2bf0cca99ad7cd1ab291a9d
aNNiPAk <annipak@yandex.ru>
2023-07-08 22:24:32 +0300

    feat: use generate models from api

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,45 +1,17 @@
 import { Component } from '@angular/core';
+import { FlightService } from '../api/services/flight.service';
+import { FlightRm } from '../api/models';
 
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
-  styleUrls: ['./search-flights.component.css']
+  styleUrls: ['./search-flights.component.css'],
 })
 export class SearchFlightsComponent {
-  searchResult: IFlightRm[] = [
-    {
-      airline: 'American Airlines',
-      remainingNumberOfSeats: 500,
-      departure: { time: Date.now().toString(), place: 'Los Angeles' },
-      arrival: { time: Date.now().toString(), place: 'Istanbul' },
-      price: '350'
-    },
-    {
-      airline: 'Deutsche BA',
-      remainingNumberOfSeats: 60,
-      departure: { time: Date.now().toString(), place: 'Munchen' },
-      arrival: { time: Date.now().toString(), place: 'Schiphol' },
-      price: '600'
-    },
-    {
-      airline: 'British Airways',
-      remainingNumberOfSeats: 60,
-      departure: { time: Date.now().toString(), place: 'London, England' },
-      arrival: { time: Date.now().toString(), place: 'Vizzola-Ticino' },
-      price: '600'
-    }
-  ]
-}
+  searchResult: FlightRm[] = [];
 
-export interface IFlightRm {
-  airline: string;
-  arrival: ITimePalceRm;
-  departure: ITimePalceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
-
-export interface ITimePalceRm {
-  place: string;
-  time: string;
+  constructor(private flightService: FlightService) {}
+  search(): void {
+    this.flightService.flightGet({}).subscribe((r) => (this.searchResult = r));
+  }
 }
