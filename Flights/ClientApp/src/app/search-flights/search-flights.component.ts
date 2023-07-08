6d8743cc390abc1037f80952a18acc581b2c8e5b
aNNiPAk <annipak@yandex.ru>
2023-07-08 21:03:32 +0300

    feat: creating a test data stub for search-flights

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-flights.component.css']
 })
 export class SearchFlightsComponent {
+  searchResult: IFlightRm[] = [
+    {
+      airline: 'American Airlines',
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: 'Los Angeles' },
+      arrival: { time: Date.now().toString(), place: 'Istanbul' },
+      price: '350'
+    },
+    {
+      airline: 'Deutsche BA',
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: 'Munchen' },
+      arrival: { time: Date.now().toString(), place: 'Schiphol' },
+      price: '600'
+    },
+    {
+      airline: 'British Airways',
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: 'London, England' },
+      arrival: { time: Date.now().toString(), place: 'Vizzola-Ticino' },
+      price: '600'
+    }
+  ]
+}
 
+export interface IFlightRm {
+  airline: string;
+  arrival: ITimePalceRm;
+  departure: ITimePalceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface ITimePalceRm {
+  place: string;
+  time: string;
 }
