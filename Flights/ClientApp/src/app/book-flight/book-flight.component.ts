d9427ff37fa534be5d0ff7b097eed29c52576d78
aNNiPAk <annipak@yandex.ru>
2023-07-11 14:05:09 +0300

    feat: add initial variable flight

--- Flights/ClientApp/src/app/book-flight/book-flight.component.ts
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FlightService } from '../api/services/flight.service';
+import { FlightRm } from '../api/models';
 
 @Component({
   selector: 'app-book-flight',
@@ -8,15 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookFlightComponent {
   flightId: string = 'not loaded';
+  flight: FlightRm = {};
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private flightService: FlightService
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(
-      (p) => (this.flightId = this.findFlight(p.get('flightId')))
-    );
+    this.route.paramMap.subscribe((p) => this.findFlight(p.get('flightId')));
   }
 
-  private findFlight = (flightId: string | null) =>
-    (this.flightId = flightId ?? 'not loaded');
+  private findFlight = (flightId: string | null) => {
+    this.flightId = flightId ?? 'not loaded';
+
+    this.flightService
+      .findFlight({ id: this.flightId })
+      .subscribe((flight) => (this.flight = flight));
+  };
 }
