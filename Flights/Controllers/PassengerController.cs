3e93a441ec45551d98ece6993722fb8efdbe6969
aNNiPAk <annipak@yandex.ru>
2023-07-11 16:59:57 +0300

    feat: add new passenger controller and passenger dto in backend

---
+++ Flights/Controllers/PassengerController.cs
@@ -0,0 +1,19 @@
+﻿using Flights.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flights.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PassengerController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult Register(NewPassengerDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
