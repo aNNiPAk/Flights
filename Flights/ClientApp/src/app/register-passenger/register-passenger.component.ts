b86d477b68208ea39978ab8f2e70c87aca65d4aa
aNNiPAk <annipak@yandex.ru>
2023-07-12 14:47:34 +0300

    feat: add data retrieval from registration form

--- Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
+++ Flights/ClientApp/src/app/register-passenger/register-passenger.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PassengerService } from '../api/services';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register-passenger',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-passenger.component.css']
 })
 export class RegisterPassengerComponent {
+  constructor(private passengerService: PassengerService, private fb: FormBuilder) {}
 
+  form = this.fb.group({
+    email: [''],
+    firstName: [''],
+    lastName: [''],
+    isFemale: [true]
+  });
+
+  register() {
+    console.log(this.form);
+    this.passengerService.registerPassenger();
+  }
 }
