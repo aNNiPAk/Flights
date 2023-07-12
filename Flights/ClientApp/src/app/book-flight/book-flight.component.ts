8a5abd2e16c6d3f539bbea813688f0620d0f0e05
aNNiPAk <annipak@yandex.ru>
2023-07-12 20:23:42 +0300

    feat: add mock for book method in frontend

--- Flights/ClientApp/src/app/book-flight/book-flight.component.ts
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FlightService } from '../api/services/flight.service';
 import { FlightRm } from '../api/models';
+import { AuthService } from '../auth/auth.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-book-flight',
@@ -9,16 +11,26 @@ import { FlightRm } from '../api/models';
   styleUrls: ['./book-flight.component.css'],
 })
 export class BookFlightComponent {
-  flightId: string = 'not loaded';
-  flight: FlightRm = {};
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private flightService: FlightService
+    private flightService: FlightService,
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {}
 
+  flightId: string = 'not loaded';
+  flight: FlightRm = {};
+
+  form = this.fb.group({
+    number: [1],
+  });
+
   ngOnInit() {
+    if (!this.authService.currentUser) {
+      this.router.navigate(['/register-passenger']);
+    }
+
     this.route.paramMap.subscribe((p) => this.findFlight(p.get('flightId')));
   }
 
@@ -38,4 +50,8 @@ export class BookFlightComponent {
 
     console.log(err);
   };
+
+  book() {
+    console.log(this.form.value.number, this.flightId);
+  }
 }
