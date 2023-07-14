d138ee9af541be01496fc2f083bdcfbb304d8bc4
aNNiPAk <annipak@yandex.ru>
2023-07-14 15:47:28 +0300

    fix: moving flight data to app scope

--- Flights/Program.cs
+++ Flights/Program.cs
@@ -1,4 +1,5 @@
 using Flights.Data;
+using Flights.Domain.Entities;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,46 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddSingleton<Entities>();
 
 var app = builder.Build();
+
+var entities = app.Services.CreateScope().ServiceProvider.GetService<Entities>();
+var random = new Random();
+
+Flight[] flightsToSeed =
+{
+    new(Guid.NewGuid(),
+        "American Airlines",
+        new TimePlace("Istanbul", DateTime.Now.AddHours(random.Next(4, 10))),
+        new TimePlace("Los Angeles", DateTime.Now.AddHours(random.Next(1, 3))),
+        random.Next(90, 5000).ToString(),
+        random.Next(1, 853)),
+    new(Guid.NewGuid(),
+        "Deutsche BA",
+        new TimePlace("Schiphol", DateTime.Now.AddHours(random.Next(4, 15))),
+        new TimePlace("Munchen", DateTime.Now.AddHours(random.Next(1, 10))),
+        random.Next(90, 5000).ToString(),
+        random.Next(1, 853)),
+    new(Guid.NewGuid(),
+        "British Airways",
+        new TimePlace("Vizzola-Ticino", DateTime.Now.AddHours(random.Next(4, 18))),
+        new TimePlace("London, England", DateTime.Now.AddHours(random.Next(1, 15))),
+        random.Next(90, 5000).ToString(),
+        random.Next(1, 853)),
+    new(Guid.NewGuid(),
+        "Basiq Air",
+        new TimePlace("Glasgow, Scotland", DateTime.Now.AddHours(random.Next(4, 21))),
+        new TimePlace("Amsterdam", DateTime.Now.AddHours(random.Next(1, 21))),
+        random.Next(90, 5000).ToString(),
+        random.Next(1, 853)),
+    new(Guid.NewGuid(),
+        "BB Heliag",
+        new TimePlace("Baku", DateTime.Now.AddHours(random.Next(4, 25))),
+        new TimePlace("Zurich", DateTime.Now.AddHours(random.Next(1, 23))),
+        random.Next(90, 5000).ToString(),
+        random.Next(1, 853))
+};
+
+entities?.Flights.AddRange(flightsToSeed);
+
 app.UseCors(policyBuilder => policyBuilder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod());
 app.UseSwagger().UseSwaggerUI();
 
